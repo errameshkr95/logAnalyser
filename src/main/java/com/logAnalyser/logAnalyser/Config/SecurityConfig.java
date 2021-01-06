041974700fbb1b27979822613209f430c2710a46
Ramesh Kumar <rameshatcyber@gmail.com>
2021-01-06 15:57:11 +0530

    modification for registerUser api, no token required

--- src/main/java/com/logAnalyser/logAnalyser/Config/SecurityConfig.java
+++ src/main/java/com/logAnalyser/logAnalyser/Config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .authorizeRequests().antMatchers("/authenticate","/registerUser").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
